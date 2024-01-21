152ad31d14de6ec750047fa9fea7029afdf95e8b
siyathokozaM <siya.mhlawuli13@gmail.com>
2024-01-21 23:18:19 +0200

    updated w font-size

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:262069a3186d5b91bcfd110ab97e5eecee62d082db6a5e59f17769bc7dc48fc9
-size 2892
+oid sha256:1b66b44e7b24ab802797a96fef864bb79d5f66a3affdd157b7901a9d0d7fe9bb
+size 2906
