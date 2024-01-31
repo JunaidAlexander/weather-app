83862004aadac0bee41b79e09f9af33dc286414d
Junaid Alexander <junaidalexander76@gmail.com>
2024-01-30 21:53:15 -0800

    Ui done could do a little clean up
    Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc6a71a1e676aec2da0cf1fcec73a9861c066b19f959f27447f24dca1220afcb
-size 3700
+oid sha256:f8165b250cc739ab630c9c19f38d7bc57c5ea073a0c3f94748c5eee029ea222c
+size 3745
