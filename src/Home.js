de347b3de8500466e6c65063a9967f838a520129
Junaid Alexander <junaidalexander76@gmail.com>
2024-02-20 12:05:07 -0800

    fixed the background videos
    Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,134 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f39c3a9da495abf257963bf7c92d4ddacfccf9a244dde699107b43a435f855a5
-size 3896
+import React from "react";
+import "./Home.css";
+import backgroundVideo from "../src/videos/bg.mp4";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const defaultCity = "Cape Town";
+
+  const apiKey = "4824o763a0a4e436tb063a409ba4f083";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState([]);
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false);
+
+  const getWeatherData = (city) => {
+    setLoading(true);
+    axios
+      .get(`${apiUrl}&query=${encodeURIComponent(search || defaultCity)}`)
+      .then((response) => {
+        setWeather(response.data);
+        console.log("Weather data for", city, ":", response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+
+  useEffect(() => {
+    console.log(weather);
+    if (searchButtonClicked) {
+      getWeatherData();
+    }
+  }, [searchButtonClicked]);
+
+  const handleSearchButtonClick = () => {
+    setSearchButtonClicked(true);
+    getWeatherData();
+  };
+  return (
+    <div>
+      <video autoPlay muted loop>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+
+      <div className="cardm">
+        <div className="card">
+          <div className="search">
+            <input
+              type="search"
+              placeholder="search"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button onClick={handleSearchButtonClick}>
+              <i class="fa fa-search" aria-hidden="true"></i>
+            </button>
+          </div>
+          {weather.city && (
+            <>
+              <div className="main">
+                <p>
+                  <span className="main-weather">
+                    <img
+                      className="weather-icon"
+                      src={weather.condition.icon_url}
+                      alt="Weather Icon"
+                    />
+                    {weather.temperature.current}°C
+                  </span>{" "}
+                </p>
+                <p className="city">
+                  {" "}
+                  {weather.city}, {weather.condition.description}
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+        {weather.city && (
+          <div className="card2">
+            <div className="upper">
+              <div className="humidity">
+                <div className="humiditytext">
+                  feels like <p> {weather.temperature.feels_like}</p>
+                </div>
+              </div>
+
+              <div className="air">
+                <div className="airtext">
+                  feels like <p> {weather.temperature.feels_like}°C</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="lower">
+              <div className="aqi">
+                <div className="aqitext">
+                  <p>Wind</p>
+                  <p>{weather.wind.speed} Km/h</p>
+                </div>
+              </div>
+
+              <div className="realfeel">
+                <div className="realfeeltext">
+                  <p> Humid</p>
+                  <p>{weather.temperature.humidity} %</p>
+                </div>
+              </div>
+
+              <div className="pressure">
+                <div className="pressuretext">
+                  <p>Pressure</p>
+                  <p> {weather.temperature.pressure}</p>
+                </div>
+              </div>
+
+              <div className="card3"> {weather.condition.description}</div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:92ad3f1be3a0543e40f14710ae026bdc01864271476daa4f656a38015a82b67b
-size 260
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<Home />
+	</React.StrictMode>
+);
+
